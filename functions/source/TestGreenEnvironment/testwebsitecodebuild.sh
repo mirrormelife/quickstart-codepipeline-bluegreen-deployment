8260ea1573cc2257cff17fb6929ba02dc6438a4c
Harini Nambi <harini18@gmail.com>
2021-04-03 22:50:52 -0700

    Added Green and https Testing

--- functions/source/TestGreenEnvironment/testwebsitecodebuild.sh
+++ functions/source/TestGreenEnvironment/testwebsitecodebuild.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 
-echo $BlueEnvName
-cname=$(aws elasticbeanstalk describe-environments --environment-names ${BlueEnvName} --region $AWS_REGION --query Environments[0].CNAME --output text)
+echo $GreenEnvName
+cname=$(aws elasticbeanstalk describe-environments --environment-names ${GreenEnvName} --region $AWS_REGION --query Environments[0].CNAME --output text)
 echo $cname
 if [ $cname ]
   then
       echo $( curl -LIk https://$cname )
       status=$( curl -LIk https://$cname -o /dev/null -w '%{http_code}\n' -s )
   echo $status
-  echo $BlueEnvName
+  echo $GreenEnvName
   if [ ${status} = "200" ]
       then exit 0
   else
